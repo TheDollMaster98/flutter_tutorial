6838e03e4b0dd317213abe0dc3f80b9b347d6946
TheDollMaster98 <lorisdalsanto@hotmail.it>
2024-06-19 17:53:20 +0200

    Commenti spiegazione e card piccole

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,11 +13,15 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: GridView.builder(
+          // SliverGridDelegateWithFixedCrossAxisCount è una classe delegate che controlla la disposizione degli elementi in una griglia.
+          // 'Sliver' è un termine che rappresenta una porzione scorrevole dell'interfaccia utente.
+          // In questo caso, la griglia è una sliver, cioè una sezione che può essere scorsa verticalmente.
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 16.0,
-            mainAxisSpacing: 16.0,
-            childAspectRatio: 2 / 3,
+            crossAxisCount: 2, // Numero di colonne nella griglia
+            crossAxisSpacing: 16.0, // Spaziatura orizzontale tra gli elementi
+            mainAxisSpacing: 16.0, // Spaziatura verticale tra gli elementi
+            childAspectRatio: 3 /
+                4, // Modifica il rapporto d'aspetto per rendere le card meno alte
           ),
           itemCount: sections.length,
           itemBuilder: (context, index) {
@@ -29,14 +33,16 @@ class HomePage extends StatelessWidget {
               child: Card(
                 color: Theme.of(context).cardColor,
                 child: Padding(
-                  padding: const EdgeInsets.all(16.0),
+                  padding: const EdgeInsets.all(12.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
                       Text(
                         section['title']!,
                         style: const TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                       const SizedBox(height: 10),
                       Text(
